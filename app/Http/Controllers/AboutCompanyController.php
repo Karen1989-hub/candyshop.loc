28479df62900eee71a781fc2d02638fef59081a4
Karen1989-hub <aroyan.89@inbox.ru>
2021-04-08 16:49:58 +0400

    save

--- app/Http/Controllers/AboutCompanyController.php
+++ app/Http/Controllers/AboutCompanyController.php
@@ -3,14 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AboutCompany;
+use Illuminate\Support\Facades\Cookie;
+use Validator;
 
 class AboutCompanyController extends Controller
 {
     public function updaetCompanyInfo(Request $request){
-        $title = $request->input('title');
-        $text = $request->input('text');
+        $adminKey= Cookie::get('adminKey');
+        if ($adminKey == 'ak587238') {
+            $title = $request->input('title');
+            $text = $request->input('text');
+            $file = $request->file('uploadImg');
 
+            $validator = Validator::make($request->all(),
+            ['title' => 'required | max:50',
+             'text' => 'required | max:1000',
+             'uploadImg' => 'required'
+             ],
+             [
+              'title.required' => 'поле должно быт заполненно',
+              'title.max' => 'поле должно быть не больше 50 синволов',
+              'text.required' => 'поле должно быт заполненно',
+              'text.max' => 'поле должно быть не больше 1000 синволов',
+              'uploadImg.required' => 'поле должно быт заполненно'
+             ]
+            );
 
-        return back();
+            $images = glob('images/aboutCompanyImg/*'); // get all file names
+            foreach($images as $file){ // iterate files
+                if(is_file($file)) {
+                    unlink($file);// delete file
+                }
+            }
+
+            $file->move('images/aboutCompanyImg', $file->getClientOriginalName());
+
+            $update = AboutCompany::find(1);
+            $update->title = $title;
+            $update->text = $text;
+            $update->imgName = $file->getClientOriginalName();
+            $update->save();
+
+            return back();
+        } else {
+            return abort('404');
+        }
+
     }
+    
 }
--- app/Models/AboutCompany.php
+++ app/Models/AboutCompany.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class AboutCompany extends Model
 {
     use HasFactory;
+    protected $fillable = ['title','text','imgName'];
 }
